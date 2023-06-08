31680b09afce36b0cabfd86077c86a1aac8f4886
rooneyClarusway <rooney@clarusway.com>
2023-06-08 15:16:35 +0300

    github hook

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -13,10 +13,6 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-
-
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
 import java.net.URL;
